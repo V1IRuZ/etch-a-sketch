7dd6ebbac48e60b2e9b9eb223494fbaab2614208
Esa <esa-pitkanen@hotmail.com>
2024-11-09 17:40:33 +0200

    Add a button for user input
    A button was added for the user to specify the number of squares in the grid. If the user enters a larger number than is allowed, it returns the default grid.
    
    The button and h1 elements were added to the index.html file and the styles were customized in the css file.

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
+function getSquares() {
+    let answer = prompt("Enter the number of squares you want the grid to have per side (maximum amount allowed is 50). ");
+    if (answer > 0 && answer <= 50) {
+        return answer;
+    } else {
+        return answer = 16;
+    }
+}
 
-function getGrid () {
-    let num = 16;
-    let size = 800 / num;
-    let grid = num * num;
+btn.addEventListener('click', () => {
+    const amountOfSquares = getSquares();
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    getGrid(amountOfSquares);
+    getHoverEffect();
+});
+
+function getGrid (amount = 16) {
+    let size = 800 / amount;
+    let grid = amount ** 2;
     for (box = 0; box < grid; box++) {
         const div = document.createElement("div");
         div.classList.add("box");
-        div.style.cssText = `width: ${size}px; height: ${size}; border: 2px solid black;`;
+        div.style.cssText = `width: ${size}px; height: ${size}; border: 2px`;
         container.appendChild(div);
     }
 }
 
-getGrid();
-
-const squares = document.querySelectorAll("div.box")
-squares.forEach((square) => {
-    square.addEventListener('mouseenter',(e) => {
-        e.target.style.background = "blue";
+function getHoverEffect() {
+    const squares = document.querySelectorAll("div.box");
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter',(e) => {
+            e.target.style.background = "blue";
+        });
     });
-});
+}
+
+getGrid();
+getHoverEffect();
 
