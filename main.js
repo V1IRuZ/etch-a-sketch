ab94df8714d19e5d5950828bf1ea8816dfe8e544
Esa <esa-pitkanen@hotmail.com>
2024-11-09 14:00:00 +0200

    Add a hover effect to the squares
    The hover effect was added with the forEach method when the mouse enters the square div. Also added variables for grid creation to make it easier to create a grid with the desired number of squares.

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const container = document.querySelector(".container");
 
-let grid = 16 * 16;
-for (box = 0; box < grid; box++) {
-    const div = document.createElement("div");
-    div.classList.add("box");
 
-    container.appendChild(div);
+function getGrid () {
+    let num = 16;
+    let size = 800 / num;
+    let grid = num * num;
+    for (box = 0; box < grid; box++) {
+        const div = document.createElement("div");
+        div.classList.add("box");
+        div.style.cssText = `width: ${size}px; height: ${size}; border: 2px solid black;`;
+        container.appendChild(div);
+    }
 }
+
+getGrid();
+
+const squares = document.querySelectorAll("div.box")
+squares.forEach((square) => {
+    square.addEventListener('mouseenter',(e) => {
+        e.target.style.background = "blue";
+    });
+});
 
